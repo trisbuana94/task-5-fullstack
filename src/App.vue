98956520bd373109e0f088fdf0c13f83e1ffa33b
trisbuana94 <trisnabuana.tb@gmail.com>
2023-01-24 14:43:04 +0700

    install project with components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+        <HeaderComponent />
+        <router-view />
+        <FooterComponent />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from "@/components/Header";
+import FooterComponent from "@/components/Footer";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    name: "AppComponent",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    components: {
+        HeaderComponent,
+        FooterComponent,
+    },
+};
+</script>
